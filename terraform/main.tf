39a7081e2207670856fb13334cd6b81f9f84706b
Samriddh <samriddh005@outlook.com>
2025-04-05 15:37:14 +0530

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = "appservice-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name                = var.app_service_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+
+  site_config {
+    always_on = false
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "react-rg"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "app_service_name" {
+  default = "react-app-service"
+}
