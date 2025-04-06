563256ba8892d2af33779636425b5d75a55f90a9
samriddhagarwal07 <samriddh005@gmail.com>
2025-04-06 21:30:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   features {}
-subscription_id = "4a6f3be4-14cd-451b-97b0-d315131d91cd"
+  subscription_id = "4a6f3be4-14cd-451b-97b0-d315131d91cd"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -8,22 +8,20 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   name                = "appservice-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  sku_name = "F1"
+  os_type  = "Windows"
 }
 
-resource "azurerm_app_service" "app" {
+resource "azurerm_windows_web_app" "app" {
   name                = var.app_service_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+  service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
     always_on = false
